fe5cf24a182842f23884dbdbe0e43fc25eb6124f
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-03-28 16:10:57 +0200

    updated order of projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,74 +7,74 @@ const Portfolio = () => {
       <h5>All Projects are on Github</h5>
       <h2>My Projects</h2>
       <div className="main_box">
-        <a href="https://github.com/andreas8151/Frontend-3-Store">
-          <img
-            className="portf_img"
-            src={require("../../assets/1.png")}
-            alt="About img"
-          />
-        </a>
-        <a href="https://github.com/andreas8151/Frontend-3-Store">
-          <img
-            className="portf_img"
-            src={require("../../assets/2.png")}
-            alt=""
-          />
-        </a>
-        <a href="https://github.com/andreas8151/Frontend-3-Store">
-          <img
-            className="portf_img"
-            src={require("../../assets/4.png")}
-            alt=""
-          />
-        </a>
-        <a href="https://github.com/andreas8151/finance-tracking">
-          <img
-            className="portf_img"
-            src={require("../../assets/12.png")}
-            alt=""
-          />
-        </a>{" "}
-        <a href="https://github.com/andreas8151/finance-tracking">
-          <img
-            className="portf_img"
-            src={require("../../assets/11.png")}
-            alt=""
-          />
-        </a>
         <a href="https://github.com/andreas8151/Todo-Examination">
           <img
             className="portf_img"
             src={require("../../assets/8.png")}
-            alt=""
+            alt="project img"
           />
         </a>
         <a href="https://github.com/andreas8151/Todo-Examination">
           <img
             className="portf_img"
             src={require("../../assets/9.png")}
-            alt=""
+            alt="project img"
+          />
+        </a>
+        <a href="https://github.com/andreas8151/Frontend-3-Store">
+          <img
+            className="portf_img"
+            src={require("../../assets/1.png")}
+            alt="project img"
+          />
+        </a>
+        <a href="https://github.com/andreas8151/Frontend-3-Store">
+          <img
+            className="portf_img"
+            src={require("../../assets/2.png")}
+            alt="project img"
+          />
+        </a>
+        <a href="https://github.com/andreas8151/Frontend-3-Store">
+          <img
+            className="portf_img"
+            src={require("../../assets/4.png")}
+            alt="project img"
+          />
+        </a>
+        <a href="https://github.com/andreas8151/finance-tracking">
+          <img
+            className="portf_img"
+            src={require("../../assets/12.png")}
+            alt="project img"
+          />
+        </a>{" "}
+        <a href="https://github.com/andreas8151/finance-tracking">
+          <img
+            className="portf_img"
+            src={require("../../assets/11.png")}
+            alt="project img"
           />
         </a>
         <a href="https://github.com/andreas8151/netflux">
           <img
             className="portf_img"
             src={require("../../assets/5.png")}
-            alt=""
+            alt="project img"
           />
         </a>
         <a href="https://github.com/andreas8151/h-m-store">
           <img
             className="portf_img"
             src={require("../../assets/6.png")}
-            alt=""
+            alt="project img"
           />
         </a>
         <a href="https://github.com/andreas8151/HTML-CSS-examination">
           <img
             className="portf_img"
             src={require("../../assets/7.png")}
-            alt=""
+            alt="project img"
           />
         </a>
       </div>
