adb6877826292cab08614642a7520b0deb9b59a5
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-03-28 16:09:49 +0200

    added cv  to navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./nav.css";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
 
 const Nav = () => {
   return (
@@ -14,6 +15,14 @@ const Nav = () => {
           rel="noreferrer"
         >
           <BsLinkedin className="header_socials" />
+        </a>
+        <a
+          className="linkTo"
+          href="https://user-images.githubusercontent.com/112560055/226950735-7dcb385c-51e1-4a40-ae70-7974de9b6e71.png"
+          target="_blank"
+        >
+          <FaFileAlt className="header_socials" />
+          <small>CV</small>
         </a>
         <a
           className="linkTo"
